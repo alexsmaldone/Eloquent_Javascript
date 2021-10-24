d3be2286dcb1769ca1943ae23d18f344ff2d7842
alexsmaldone <alexsmaldone@gmail.com>
2021-10-24 15:09:21 -0400

    added buildGraph function that creates a map object that, for each n, creates an array of connected n's

--- Proj_Robot/robot.js
+++ Proj_Robot/robot.js
@@ -1,3 +1,6 @@
+//Setting up the array with our town of Meadowfield, which has 11 locations with 14 roads between them.
+//This network of roads forms a graph, which is a collection of points (places in the village) with lines between them (roads).
+
 const roads = [
   "Alice's House-Bob's House",   "Alice's House-Cabin",
   "Alice's House-Post Office",   "Bob's House-Town Hall",
@@ -8,4 +11,26 @@ const roads = [
   "Marketplace-Town Hall",       "Shop-Town Hall"
 ];
 
-console.log(roads);
+//Converting the array to a data structure that, for each place, can tell us what can be reached from there
+//Given an array of edges, the buildGraph function creates a map object that, for each node, stores an array of connected nodes
+//It uses the split method to go from the road strings, which have the form "Start=End", to two-element arrays containing the start and end as a separate string
+
+function buildGraph(edges) {
+  let graph = Object.create(null);
+  function addEdge(from, to) {
+    if (graph[from] == null) {
+      graph[from] = [to];
+    } else {
+      graph[from].push(to);
+    }
+  }
+  for (let [from, to] of edges.map(r => r.split("-"))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+  return graph;
+}
+
+const roadGraph = buildGraph(roads);
+
+console.log(roadGraph);
