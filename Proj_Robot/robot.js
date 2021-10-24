d659eb37ecafb200edfd265dbaff510efd7afb4b
alexsmaldone <alexsmaldone@gmail.com>
2021-10-24 17:06:49 -0400

    added pathfinding robot

--- Proj_Robot/robot.js
+++ Proj_Robot/robot.js
@@ -125,6 +125,69 @@ VillageState.random = function(parcelCount = 5) {
   return new VillageState("Post Office", parcels);
 }
 
-runRobot(VillageState.random(), randomRobot);
+
 
 // We should be able to do better than a random robot. An easy improvement would be to find a route that passes all places in the village and run that route
+//Mail Truck's Route
+
+const mailRoute = [
+  "Alice's House", "Cabin", "Alice's House", "Bob's House",
+  "Town Hall", "Daria's House", "Ernie's House",
+  "Grete's House", "Shop", "Grete's House", "Farm",
+  "Marketplace", "Post Office"
+];
+
+function routeRobot(state, memory) {
+  if (memory.length == 0) {
+    memory = mailRoute;
+  }
+  return {direction: memory[0], memory: memory.slice(1)};
+}
+
+/*
+A pathfinding robot would be more intelligent than a robot that follows a simple fixed route like the robot above.
+The problem of finding a route through a graph is a typical search problem.
+We can tell whether a given solution (a route) is a valid solution, but we can’t directly compute the solution the way we could for 2 + 2.
+Instead, we have to keep creating potential solutions until we find one that works.
+The number of possible routes through a graph is infinite. But when searching for a route from A to B, we are interested only in the ones that start at A.
+We also don’t care about routes that visit the same place twice—those are definitely not the most efficient route anywhere.
+So that cuts down on the number of routes that the route finder has to consider.
+In fact, we are mostly interested in the shortest route. So we want to make sure we look at short routes before we look at longer ones.
+A good approach would be to “grow” routes from the starting point, exploring every reachable place that hasn’t been visited yet, until a route reaches the goal.
+That way, we’ll only explore routes that are potentially interesting, and we’ll find the shortest route (or one of the shortest routes, if there are more than one) to the goal.
+*/
+function findRoute(graph, from, to) {
+  let work = [{at: from, route: []}];
+  for (let i = 0; i < work.length; i++) {
+    let {at, route} = work[i];
+    for (let place of graph[at]) {
+      if (place == to) return route.concat(place);
+      if (!work.some(w => w.at == place)) {
+        work.push({at: place, route: route.concat(place)});
+      }
+    }
+  }
+}
+
+
+
+/*
+This robot uses its memory value as a list of directions to move in, just like the route-following robot. Whenever that list is empty, it has to figure out what to do next.
+It takes the first undelivered parcel in the set and, if that parcel hasn’t been picked up yet, plots a route toward it.
+If the parcel has been picked up, it still needs to be delivered, so the robot creates a route toward the delivery address instead.
+*/
+
+function goalOrientedRobot({place, parcels}, route) {
+  if (route.length == 0) {
+    let parcel = parcels[0];
+    if (parcel.place != place) {
+      route = findRoute(roadGraph, place, parcel.place);
+    } else {
+      route = findRoute(roadGraph, place, parcel.address);
+    }
+  }
+  return {direction: route[0], memory: route.slice(1)};
+}
+
+runRobot(VillageState.random(),
+                  goalOrientedRobot, []);
