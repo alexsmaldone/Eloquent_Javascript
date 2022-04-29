a842ee529ba52113fbaad54f72f07f2df52b367d
alexsmaldone <alexsmaldone@gmail.com>
2022-04-28 22:59:38 -0400

    added functions

--- Programming_Language/programming_language.js
+++ Programming_Language/programming_language.js
@@ -173,6 +173,70 @@ for (let op of ["+", "-", "*", "/", "==", "<", ">"]) {
   topScope[op] = Function("a, b", `return a ${op} b;`);
 }
 
+// Print Function
+topScope.print = (value) => {
+  console.log(value);
+  return value;
+};
+
+// Parse program and run it in fresh scope
+function run(program) {
+  return evaluate(parse(program), Object.create(topScope));
+}
+
+// Test for scoping/running a program
+
+// run(`
+// do(define(total, 0),
+//    define(count, 1),
+//    while(<(count, 11),
+//          do(define(total, +(total, count)),
+//             define(count, +(count, 1)))),
+//    print(total))
+// `);
+// → 55
+
 // let prog = parse(`if(true, false, true)`);
 // console.log(evaluate(prog, topScope));
 // → false
+
+// FUNCTIONS
+specialForms.fun = (args, scope) => {
+  if (!args.length) {
+    throw new SyntaxError("Functions need a body");
+  }
+  let body = args[args.length - 1];
+  let params = args.slice(0, args.length - 1).map((expr) => {
+    if (expr.type != "word") {
+      throw new SyntaxError("Parameter names must be words");
+    }
+    return expr.name;
+  });
+
+  return function () {
+    if (arguments.length != params.length) {
+      throw new TypeError("Wrong number of arguments");
+    }
+    let localScope = Object.create(scope);
+    for (let i = 0; i < arguments.length; i++) {
+      localScope[params[i]] = arguments[i];
+    }
+    return evaluate(body, localScope);
+  };
+};
+
+// FUNCTION TESTING
+run(`
+do(define(plusOne, fun(a, +(a, 1))),
+   print(plusOne(10)))
+`);
+// → 11
+
+run(`
+do(define(pow, fun(base, exp,
+     if(==(exp, 0),
+        1,
+        *(base, pow(base, -(exp, 1)))))),
+   print(pow(2, 10)))
+`);
+// → 1024
