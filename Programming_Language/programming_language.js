e3e9b3874ee38cac409959cd1ea5e5ada94be28e
alexsmaldone <alexsmaldone@gmail.com>
2022-04-28 22:32:09 -0400

    creating a while loop

--- Programming_Language/programming_language.js
+++ Programming_Language/programming_language.js
@@ -125,3 +125,16 @@ specialForms.if = (args, scope) => {
     return evaluate(args[2], scope);
   }
 };
+
+specialForms.while = (args, scope) => {
+  if (args.length != 2) {
+    throw new SyntaxError("Wrong number of args to while");
+  }
+  while (evaluate(args[0], scope) !== false) {
+    evaluate(args[1], scope);
+  }
+
+  // Since undefined does not exist in Egg, we return false,
+  // for lack of a meaningful result.
+  return false;
+};
