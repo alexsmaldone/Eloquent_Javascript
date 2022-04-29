0a2fcb03bf24e530b3ce02b63282ef3d29f29efd
alexsmaldone <alexsmaldone@gmail.com>
2022-04-28 22:30:33 -0400

    creating an if statement

--- Programming_Language/programming_language.js
+++ Programming_Language/programming_language.js
@@ -115,3 +115,13 @@ function evaluate(expr, scope) {
 The specialForms object is used to define special syntax in Egg.
 It associates words with functions that evaluate such forms. It is currently empty. Let’s add if.
 */
+
+specialForms.if = (args, scope) => {
+  if (args.length != 3) {
+    throw new SyntaxError("Wrong number of args to if");
+  } else if (evaluate(args[0], scope) !== false) {
+    return evaluate(args[1], scope);
+  } else {
+    return evaluate(args[2], scope);
+  }
+};
