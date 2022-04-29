fc4c9c35081abe527c3f9c67724abc0de42d297e
alexsmaldone <alexsmaldone@gmail.com>
2022-04-28 22:29:26 -0400

    specialforms evaluator object

--- Programming_Language/programming_language.js
+++ Programming_Language/programming_language.js
@@ -77,4 +77,41 @@ function parse(program) {
   return expr;
 }
 
-console.log(parse("+(a, 10)"));
+// console.log(parse("+(a, 10)"));
+
+/*
+What can we do with the syntax tree for a program? Run it, of course! And that is what the evaluator does.
+You give it a syntax tree and a scope object that associates names with values,
+and it will evaluate the expression that the tree represents and return the value that this produces.
+*/
+
+const specialForms = Object.create(null);
+
+function evaluate(expr, scope) {
+  if (expr.type == "value") {
+    return expr.value;
+  } else if (expr.type == "word") {
+    if (expr.name in scope) {
+      return scope[expr.name];
+    } else {
+      throw new ReferenceError(`Undefined bindind: ${expr.name}`);
+    }
+  } else if (expr.type == "apply") {
+    let { operator, args } = expr;
+    if (operator.type == "word" && operator.name in specialForms) {
+      return specialForms[operator.name](expr.args, scope);
+    } else {
+      let op = evaluate(operator, scope);
+      if (typeof op == "function") {
+        return op(...args.map((arg) => evaluate(arg, scope)));
+      } else {
+        throw new TypeError("Applying a non-function.");
+      }
+    }
+  }
+}
+
+/*
+The specialForms object is used to define special syntax in Egg.
+It associates words with functions that evaluate such forms. It is currently empty. Let’s add if.
+*/
