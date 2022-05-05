863720834ac2436be035d605dd069d6870a52a17
alexsmaldone <alexsmaldone@gmail.com>
2022-05-05 17:59:13 -0400

    fixed scoping issues

--- Programming_Language/programming_language.js
+++ Programming_Language/programming_language.js
@@ -159,6 +159,23 @@ specialForms.define = (args, scope) => {
   return value;
 };
 
+// Fixing SCOPE
+specialForms.set = (args, scope) => {
+  if (args.length != 2 || args[0].type != "word") {
+    throw new SyntaxError("Bad use of set");
+  }
+  let varName = args[0].name;
+  let value = evaluate(args[1], env);
+
+  for (let scope = env; scope; scope = Object.getPrototypeOf(scope)) {
+    if (Object.prototype.hasOwnProperty.call(scope, varName)) {
+      scope[varName] = value;
+      return value;
+    }
+  }
+  throw new ReferenceError(`Setting undefined variable ${varName}`);
+};
+
 // Global Variables
 
 const topScope = Object.create(null);
